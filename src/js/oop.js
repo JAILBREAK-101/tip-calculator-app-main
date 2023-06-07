c0c49eedd104cb767651ffc7bd5575374dc1ea3e
Oluwagbemiga <samuelifeoluwa540@gmail.com>
2023-06-08 00:24:27 +0200

    set reset and UI changes

--- src/js/oop.js
+++ src/js/oop.js
@@ -5,6 +5,8 @@ let billAmount = document.querySelector("[data-bill-amount]");
 
 let noOfPeople = document.querySelector("[data-no-of-people]");
 
+let customTip = document.querySelector("[data-custom-tip]");
+
 let errorMessage = document.querySelector("[data-error-msg]");
 
 let tipAmount = document.querySelector("[data-tip-value]");
@@ -16,17 +18,18 @@ totalAmount.innerText = initialVal;
 const tipSelects = document.querySelectorAll("[data-grid-button]");
 
 const resetButton = document.querySelector("[data-reset-button]");
+resetButton.disabled = true;
 
 class TipCalculator {
-  constructor(initialValue, bill, people, tip, total) {
+  constructor(initialValue, bill, people, tip, total, custom) {
     this.initialValue = initialValue;
     this.bill = bill;
     this.people = people;
     this.tip = tip;
     this.total = total;
+    this.custom = custom;
   }
 
-  // Getters and setters
   /**
    * @param {(arg0: any) => void} billAmount
    */
@@ -60,17 +63,10 @@ class TipCalculator {
 
   // Pure Methods
   get calculateTipAmount() {
-    // Object.defineProperty(CalculatorApp, "setTotalAmount", {
-    //   set: {
-    //     total: eval(this.bill / this.people + this.tip).toPrecision(4),
-    //   },
-    // });
     this.setTipAmount(
       (this.tip =
         (eval(this.bill / this.people) / 100) * this.selectedTip).toPrecision(4)
-    ); // setters should only be accessed outside the class, but not called as methods inside or outside the class, same as getters
-
-    this.setTotalAmount((this.total = this.bill / this.people + this.tip));
+    );
   }
 
   pickSelect(select) {
@@ -94,8 +90,6 @@ class TipCalculator {
   // Number Input Validation
 
   checkInputs(...inputs) {
-    // Number Input validation
-
     inputs.forEach((input) => {
       input.addEventListener("keypress", (e) => {
         console.log(e);
@@ -114,8 +108,9 @@ class TipCalculator {
       CalculatorApp.bill = "";
       CalculatorApp.people = "";
       CalculatorApp.selectedTip = "";
-      CalculatorApp.tip = undefined;
-      CalculatorApp.total = undefined;
+      CalculatorApp.tip = "0";
+      CalculatorApp.total = "0";
+      CalculatorApp.custom = "";
       try {
         value.innerText = initialVal;
       } catch (err) {
@@ -137,22 +132,25 @@ const inputLength = (input) => {
   }
 };
 
-// The Class Created App
+const setDisabled = (input) => {
+  input.length > 0 ? !resetButton.disabled : "";
+};
+
 const CalculatorApp = new TipCalculator(
   initialVal,
   billAmount.value,
   noOfPeople.value,
   initialVal,
-  initialVal
+  initialVal,
+  customTip.value
 );
-// Add Event Listener
+
 billAmount.addEventListener("keyup", (e) => {
-  inputLength(billAmount);
+  console.log(e.target.value.length);
   if (e.target.value.includes(e.target.value)) {
     CalculatorApp.setBillAmount = e.target.value.slice(
       e.target.value.length - 1
     );
-    // If the user cancels a value
     if (e.key === "Backspace") {
       CalculatorApp.bill = e.target.value.substr(0, e.target.value.length);
     }
@@ -162,19 +160,12 @@ billAmount.addEventListener("keyup", (e) => {
 });
 
 noOfPeople.addEventListener("keyup", (e) => {
-  // CalculatorApp.setNoOfPeople = !isNaN(e.target.value) ? e.target.value : "";
-  // CalculatorApp.setNoOfPeople = !isNaN(e.target.value) ? e.target.value : "";
-  inputLength(noOfPeople);
+  setDisabled(e.target.value.length);
   if (e.target.value.includes(e.target.value)) {
     CalculatorApp.setNoOfPeople = e.target.value.slice(
       e.target.value.length - 1
     );
-    // If the user cancels a value
     if (e.key === "Backspace") {
-      // CalculatorApp.setNoOfPeople = e.target.value.slice(
-      //   0,
-      //   e.target.value.indexOf([e.target.value.length])
-      // );
       CalculatorApp.people = e.target.value.substr(0, e.target.value.length);
     }
   }
@@ -184,7 +175,7 @@ noOfPeople.addEventListener("keyup", (e) => {
 
 tipSelects.forEach((select) => {
   select.addEventListener("click", () => {
-    // select.classList.toggle("control-grid-select-focus");
+    select.classList.toggle("control-grid-select-focus");
     CalculatorApp.pickSelect(select);
     CalculatorApp.calculateTipAmount;
     CalculatorApp.displayResults();
@@ -192,7 +183,13 @@ tipSelects.forEach((select) => {
 });
 
 resetButton.addEventListener("click", () =>
-  CalculatorApp.resetInputs(billAmount, noOfPeople, tipAmount, totalAmount)
+  CalculatorApp.resetInputs(
+    billAmount,
+    noOfPeople,
+    tipAmount,
+    totalAmount,
+    customTip
+  )
 );
 
 CalculatorApp.displayResults();
